c58a01f35e39c0d9eb31422274340fe569a686b2
Himal Rawal <himal.rawal@8squarei.com>
2024-07-23 16:27:27 +0545

    feat:initialize ui for blog app

---
+++ blogui/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BlogPostHomePage(),
+    );
+  }
+}
+
+class BlogPostHomePage extends StatelessWidget {
+  const BlogPostHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Himal's Blog"),
+      ),
+      body: const SingleChildScrollView(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
